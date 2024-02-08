f4bd704b16568f0f69683539b46f80aee815e81c
Davit-Abrahamyan <david.abrahamyan@ucraft.com>
2024-02-08 00:19:31 -0800

    resolved the images not displaying issue as well as broken links from home page to other pages

--- app/dcrm/settings.py
+++ app/dcrm/settings.py
@@ -24,7 +24,9 @@ email_host_password = os.environ.get("EMAIL_HOST_PASSWORD")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / "website" / "templates" / "images",  # Assuming your image is in the 'dcrm' directory
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
