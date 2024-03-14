560d650ee229f919be99aba4a593e971d092f9bb
Davit-Abrahamyan <david.abrahamyan@ucraft.com>
2024-03-14 12:14:41 -0700

    small fix

--- app/website/urls.py
+++ app/website/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
     path('create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page'),
-    path("chatbot/", views.chatbot_view, name="chatbot"),
+    path("chatbot", views.chatbot_view, name="chatbot"),
     path('reset_password/', ratelimit(key='ip', method='POST', rate = '8/5h', group='b')(auth_views.PasswordResetView.as_view(template_name = "password_reset.html", 
     email_template_name = "password_reset_email.html", subject_template_name = "password_reset_subject.txt")), name = 'password_reset'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "password_reset_sent.html"), name = 'password_reset_done'),
