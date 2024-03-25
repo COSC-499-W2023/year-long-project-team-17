a53f96d34306a5e63baabc73f5f2b9c54d324bd7
harshg03 <71566563+harshg03@users.noreply.github.com>
2024-03-24 21:38:12 -0700

    Updated edit profile, added change password and testing

--- app/website/urls.py
+++ app/website/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path("detect_plagiarism/", views.detect_plagiarism_view, name="detect_plagiarism"),
     path("generate_exercise/", views.generate_exercise_view, name="generate_exercise"),
     path("generate_adapted_content/", views.generate_adapted_content_view, name="generate_adapted_content"),
-    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
     path('create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page'),
     path("chatbot", views.chatbot_view, name="chatbot"),
@@ -44,5 +44,6 @@ urlpatterns = [
     path('presentation_status/', views.presentation_status, name='presentation_status'),
     path('exercise_status/', views.exercise_status, name="exercise_status"),
     path('exercise_loading_page', views.exercise_loading_page_view, name="exercise_loading_page"),
-    path('get_exercise', views.get_exercise_view, name="get_exercise")
+    path('get_exercise', views.get_exercise_view, name="get_exercise"),
+    path('password/', views.change_password, name='change_password'),
 ]
